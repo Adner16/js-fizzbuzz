2f1ed22d2e43791f54c72fd70f3a6f91bb813845
Adner16 <giovanni.renda16@gmail.com>
2021-12-22 15:48:31 +0100

    added js

---
+++ js/script.js
@@ -0,0 +1,22 @@
+console.log('JS OK')
+
+/*
+1 creare un ciclo che genera numeri da 1 a 100
+2 individuare i multipli di 3 e 5
+3 assegnare valori diversi ai numeri
+*/
+
+
+
+for (let i = 1; i <= 100; i++){
+    console.log (i);
+}
+if ( %3 === 0) {
+    i = 'Fizz'
+}
+if else ( %5 === 0) {
+    i = 'Buzz'
+}
+if else ( %3 %5 === 0) {
+    i = 'FizzBuzz'
+}
